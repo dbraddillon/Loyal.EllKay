cedc992d287d7bd8d34b8999ca2c0aa854885dc1
Donald Bradley Dillon <d.brad.dillon@gmail.com>
2025-01-15 11:21:26 -0500

    get code

--- Loyal.EllKay.App/Program.cs
+++ Loyal.EllKay.App/Program.cs
@@ -1,16 +1,21 @@
 ﻿using Loyal.EllKay.App;
 using RestSharp;
 
+//https://github.com/dbraddillon/Loyal.EllKay Share or move to Loyal...
+
 const string baseUrl = "https://services.lkstaging.com";
 //const string baseUrl = "https://services.lkcloud.com";
+
 const string tokenEndpoint = "https://auth2.lkidentity.com/connect/token";
+//token metadata endpoint 
+//https://fhir.lkstaging.com/R4/metadata?_format=json
 
 Console.WriteLine("Let's go.....");
 
 var config = Config.BindFromEnvironment();
 
-var client = new RestClient(tokenEndpoint);
-var request = new RestRequest(baseUrl, Method.Post);
+var client = new RestClient();
+var request = new RestRequest(tokenEndpoint, Method.Post);
 
 request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
 request.AddParameter("grant_type", "client_credentials");
